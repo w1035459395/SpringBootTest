21bc94ad80bdf0b021539e7b9ea6bac4c389d9ee
w1035459395 <43872031+w1035459395@users.noreply.github.com>
2018-10-05 17:36:41 +0800

    Update SpringBootTestApplicationTests.java

--- src/test/java/com/example/demo/SpringBootTestApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootTestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootTestApplicationTests {
+public class SpringBootTestApplicationTests1 {
 
 	@Test
 	public void contextLoads() {
